e8a86d9b02a63aeadf4dfc612f764d9fe5218dd3
parvez kose <flamboyanz@gmail.com>
2015-12-19 00:46:53 -0500

    pub-sub observer

---
+++ pub-sub-observer.js
@@ -0,0 +1,38 @@
+//Pub-Sub Pattern
+/*-------------*/
+
+(function($) {
+
+  // type 1
+
+  var o = $({});
+
+    $.each({
+        on      : 'subscribe',
+        trigger : 'publish',
+        off     : 'unsubscribe'
+
+    }, function( key, api ) {
+        $[api] = function() {
+            o[key].apply( o, arguments );
+
+        };
+    });
+
+  // type 2
+ 
+  var o = $({});
+ 
+  $.subscribe = function() {
+    o.on.apply(o, arguments);
+  };
+ 
+  $.unsubscribe = function() {
+    o.off.apply(o, arguments);
+  };
+ 
+  $.publish = function() {
+    o.trigger.apply(o, arguments);
+  };
+ 
+}(jQuery));
